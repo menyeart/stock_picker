e45d106bbd2d3ab8caae36d51840054e6d3ab9ac
Matt Enyeart <menyeart1@gmail.com>
2022-11-15 11:06:35 -0700

    Add working code for a basic stock picker

--- picker.rb
+++ picker.rb
@@ -8,3 +8,25 @@
 # stock_picker([17,3,6,9,15,8,6,1,10])
 #   => [1,4]  # for a profit of $15 - $3 == $12
 
+
+price_list = [17,3,6,9,15,8,6,1,10]
+new_hash = Hash.new([])
+
+price_list.each_with_index do |price, index1|
+  price_list.each_with_index do |next_price, index2|
+    if index2 > index1
+      profit = price - next_price
+      new_hash[profit] = [index1, index2]
+    end
+  end
+end
+
+
+
+p new_hash
+p new_hash.keys.max
+
+p new_hash.select {|key| key.to_i == new_hash.keys.max}
+
+
+
